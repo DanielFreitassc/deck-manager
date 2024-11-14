78ec495806b1314e4a19617e78d13b8a7a2f67c9
danielfreitassc <danielfreitassc@gmail.com>
2024-11-14 14:40:49 -0300

    Feat: exemplo de machine learning com interface web

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import sklearn.linear_model import LinearRegression()
+from sklearn.linear_model import LinearRegression
+
 df = pd.read_csv("pizzas.csv")
 
 
@@ -9,5 +10,13 @@ from sklearn import linear_model
 modelo = LinearRegression()
 x = df[["diametro"]]
 y = df[["preco"]]
-
 modelo.fit(x,y)
+
+st.title("Prevendo o valor de uma pizza")
+st.divider()
+
+diametro = st.number_input("Digite o tamanho do diâmetro da pizza:  ")
+
+if diametro:
+    preco_previsto = modelo.predict([[diametro]])[0][0]
+    st.write(f"O valor da pizza com diâmetro de {diametro:.2f} é de R${preco_previsto:.2f}.")
