a2f7d098ea7ce37ad6436445c55b5e12902accee
DanielFreitassc <danielfreitassc@gmail.com>
2024-11-14 11:36:22 -0300

    Feat: criando interface web

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+import sklearn.linear_model import LinearRegression()
+df = pd.read_csv("pizzas.csv")
+
+
+from sklearn import linear_model
+
+modelo = LinearRegression()
+x = df[["diametro"]]
+y = df[["preco"]]
+
+modelo.fit(x,y)
