200c86b19617a9b4338e64306fb8ce84533eb9cf
DanielFreitassc <danielfreitassc@gmail.com>
2025-02-11 19:20:19 -0300

    Inicialização do projeto Spring Boot (mvn archetype)

---
+++ src/main/java/com/danielfreitassc/deck_manager/DeckManagerApplication.java
@@ -0,0 +1,13 @@
+package com.danielfreitassc.deck_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckManagerApplication.class, args);
+	}
+
+}
