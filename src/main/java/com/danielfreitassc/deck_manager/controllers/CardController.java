3fd93d4ad4400c5676ce0dca77c9406aa8835c0e
DanielFreitassc <danielfreitassc@gmail.com>
2025-02-12 00:06:24 -0300

    Feat: Adicionando status correto ao create

--- src/main/java/com/danielfreitassc/deck_manager/controllers/CardController.java
+++ src/main/java/com/danielfreitassc/deck_manager/controllers/CardController.java
@@ -2,6 +2,7 @@ package com.danielfreitassc.deck_manager.controllers;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danielfreitassc.deck_manager.dtos.CardRequestDto;
@@ -25,6 +27,7 @@ public class CardController {
     private final CardService cardService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public CardResponseDto create(@ModelAttribute @Valid CardRequestDto cardRequestDto) throws Exception {
         return cardService.create(cardRequestDto);
     }
